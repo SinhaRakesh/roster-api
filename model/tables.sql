17f65f9b94926205e44bf17db83d4bc6f4a862e3
sinharakesh <rksinha.btech@gmail.com>
2025-06-04 10:28:21 +0530

    insert record into db

---
+++ model/tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "testimonials" (
+    "id" SERIAL,
+    "user_id" INTEGER NOT NULL,
+    "author" VARCHAR(250) NULL,
+    "text" TEXT NULL,
+    CONSTRAINT "PK_testimonials" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "users" (
+    "id" SERIAL,
+    "name" VARCHAR(250) NULL,
+    "title" VARCHAR(250) NOT NULL,
+    "email" VARCHAR(250) NULL,
+    "mobile" CHARACTER(20) NULL,
+    "portfolio_link" VARCHAR(250) NULL,
+    "skill_summary" INTEGER NULL,
+    "experience_years" INTEGER NULL,
+    "experience_details" TEXT NULL,
+    "experience_clients" JSON NULL,
+    "additional_info" JSON NULL,
+    CONSTRAINT "PK_users" PRIMARY KEY ("id")
+);
